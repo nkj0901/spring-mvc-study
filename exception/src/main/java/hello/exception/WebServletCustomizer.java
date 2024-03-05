0b7a49b1277457616e7b74bb5d88aa8ba2d20363
nam ki jung <nkj0901@naver.com>
2024-03-05 09:11:53 +0900

    20240305 예외처리와 오류페이지

---
+++ exception/src/main/java/hello/exception/WebServletCustomizer.java
@@ -0,0 +1,22 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+//에러페이지 등록
+//@Component
+public class WebServletCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        //RuntimeException의 자식들을 모두 "/error-page/500으로 보내준다.
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
